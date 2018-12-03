7f9dcef10486ac73506c388de9ebf7c68cd735ef
605186386@qq.com <605186386@qq.com>
2018-12-03 09:29:13 +0800

    党建要闻详情

--- src/app/admin/partyNews/partyNews.module.ts
+++ src/app/admin/partyNews/partyNews.module.ts
@@ -3,12 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { ShareModule } from '../../share/share.module';
 
 import { IndexComponent } from './index/index.component';
+import { SeeComponent } from './see/see.component';
 
 
 export const routes: Routes = [
     {
         path: '',
         component: IndexComponent,
+        data: {
+            name: '党建要闻',
+        }
+    },
+    {
+        path: 'see/:id',
+        component: SeeComponent,
         data: {
             name: '党建要闻',
         }
@@ -21,7 +29,7 @@ export const routes: Routes = [
         ShareModule,
         RouterModule.forChild(routes)
     ],
-    declarations: [IndexComponent],
+    declarations: [IndexComponent, SeeComponent],
     providers: [],
 })
 export class PartyNewsModule { }
